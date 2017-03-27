82091fb4116e6f900024e44890fb3daa8fc497ea
igorworkoholics <igor@workoholics.es>
2017-03-27 12:13:47 +0200

    CS select y CS radio ajustes

--- app/scripts/custom-radio.js
+++ app/scripts/custom-radio.js
@@ -11,20 +11,21 @@
             else{
                 $radio.prepend('<input type="radio" class="value">');
             }
-             
-
-        	if($radio.attr('data-checked')){
-        			
-        	}
-            else{
-
-            }
         	
+            if($radio.attr('data-checked') === 'checked'){
+                selectOption($radio);
+            }
+
         	/*  ------------ Events ------------- */
 
         	/* Click en el botón de seleccionar radio*/
         	$radio.on('click',function(e){
-        	   selectOption($radio);
+                if(!$radio.hasClass('checked')){
+                    selectOption($radio);
+                }
+                else{
+                    deselectOption($radio);
+                }
             });
         });
 
@@ -36,6 +37,11 @@
             $('[data-group="' + $radio.attr("data-group") + '"]').removeClass('checked');
             $radio.addClass('checked');
             $radio.find('.value').prop('checked',true);
+        }
+        /* Deseleccionar opción */
+        function deselectOption($radio){
+            $radio.removeClass('checked');
+            $radio.find('.value').prop('checked',false);
         }
 
     };
